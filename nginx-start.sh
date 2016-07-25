bf0a8de94ad23479e010c09b0e5bcf8da15af2ec
connorgr <connor@cs.brown.edu>
2016-07-25 11:35:58 -0400

    Add last missing resources (e.g., paper), update links, and revise instructions.

--- nginx-start.sh
+++
@@ -1,6 +0,0 @@
-python run.py --server --port 8010 &
-python run.py --server --port 8011 &
-python run.py --server --port 8012 &
-python run.py --server --port 8013 &
-python run.py --server --port 8014 &
-sudo nginx -c /Users/connor/brown/perception/colorgorical/colorgorical-nginx.conf 
